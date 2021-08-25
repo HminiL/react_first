747cf5f5cb0b0e43379d257a86c8599710636e33
hyeminLee7 <hmin10580@gmail.com>
2021-08-25 10:18:01 +0900

    Aug 25

---
+++ src/modern-javascript/chap-01-basic/basic.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+var man = {} //es5
+
+const Basic = () => {    
+    let tom = 'Tom' //es6에서 variable
+    const james = 'James' //es6에서 constant
+
+    return (<></>)
+}
+
+export default Basic
